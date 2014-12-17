37ce8c4efe1d32b1771483ad50ea33ed4c2cea3d
hackersun <hackersungl@gmail.com>
2014-12-17 23:32:27 +0800

    Find Peak Element

---
+++ Find Peak Element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) {//stupid O(n), compare 2n times.
+        int sz = num.size();
+		for(int index = 0; index < sz; index++)
+		{
+			bool isleft = true;
+			bool isright = true;
+			if(index > 0) isleft = num[index-1] < num[index] ? true : false;
+			if(index <= sz-2) isright = num[index] > num[index+1] ? true : false;
+			if(isleft && isright) return index;
+		}
+		return -1;
+    }
+};
+
+int findPeakElement(const vector<int> &num) {//smart O(n), compare n times.
+        for(int i=1; i<num.size(); i++){
+            if(num[i] < num[i-1])
+                return i-1;
+        }
+        return num.size()-1;
+    }
