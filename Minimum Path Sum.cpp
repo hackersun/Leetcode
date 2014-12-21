c799bdcc125e0f5b3868ace9f2274d9ec4a44ae4
hackersun <hackersungl@gmail.com>
2014-12-21 20:10:49 +0800

    Minimum Path Sum

---
+++ Minimum Path Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        int row = grid.size();
+		int col = grid[0].size();
+		int** dp = new int*[row];
+		for(int i = 0; i < row; i++)
+			dp[i] = new int[col];
+
+		dp[0][0] = grid[0][0];
+		for(int j = 1; j < col; j++)
+			dp[0][j] = dp[0][j-1] + grid[0][j];
+
+
+		for(int i = 1; i < row; i++)
+			dp[i][0] = dp[i-1][0] + grid[i][0];
+
+		for(int i = 1; i < row; i++)
+			for(int j = 1; j < col; j++)
+			{
+				int tmp = dp[i][j-1] < dp[i-1][j] ? dp[i][j-1] : dp[i-1][j];
+				dp[i][j] = tmp + grid[i][j];
+			}
+
+		int res = dp[row-1][col-1];
+
+		for(int i = 0; i < row; i++)
+			delete dp[i];
+		delete[] dp;
+		return res;
+    }
+};
