ec3c64319c1c7591369d32ba94137c4866ea7f79
hackersun <hackersungl@gmail.com>
2014-12-21 20:24:26 +0800

    Minimum Path Sum

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -30,3 +30,25 @@ public:
 		return res;
     }
 };
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        int row = grid.size();
+		int col = grid[0].size();
+
+		for(int j = 1; j < col; j++)
+			grid[0][j] += grid[0][j-1];
+		for(int i = 1; i < row; i++)
+			grid[i][0] += grid[i-1][0];
+
+		for(int i = 1; i < row; i++)
+			for(int j = 1; j < col; j++)
+			{
+				int tmp = grid[i-1][j] < grid[i][j-1] ? grid[i-1][j] : grid[i][j-1];
+				grid[i][j] += tmp;
+			}
+		return grid[row-1][col-1];
+    }
+};
