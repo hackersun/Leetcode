6f4e4c35d91b76a5f3043aaf9fe5d035c1e50d07
hackersun <hackersungl@gmail.com>
2015-01-04 23:43:37 +0800

    Longest Consecutive Sequence

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {//method 1, sort
 public:
     int longestConsecutive(vector<int> &num) {
 		set<int> st;
@@ -20,6 +20,41 @@ public:
 		}
 		if(curlen > maxlen) maxlen = curlen;
 
+		return maxlen;
+    }
+};
+
+class Solution {//method 2, O(n)
+public:
+    int longestConsecutive(vector<int> &num) {
+		map<int, int> nums;
+		map<int, int> visited;
+		for(int i = 0; i < num.size(); i++)
+			nums[num[i]] = 1;
+		int maxlen = 1;
+		for(int i = 0; i < num.size(); i++)
+		{
+			if(visited[num[i]] == 1) continue;
+			bool isup = true, isdown = true;
+			int up_i = num[i], down_i = num[i];
+			while(isup || isdown)
+			{
+				if(isup){
+					if(nums[up_i+1] == 1)
+						visited[++up_i] = 1;
+					else
+						isup = false;
+				}
+				if(isdown){
+					if(nums[down_i-1] == 1)
+						visited[--down_i] = 1;
+					else
+						isdown = false;
+				}
+			}
+			int tmp = up_i - down_i + 1;
+			if(tmp > maxlen) maxlen = tmp;
+		}
 		return maxlen;
     }
 };
