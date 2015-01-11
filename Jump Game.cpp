bd7a19acfbb83d4215c05717140a5f9fdb188c2a
hackersun <hackersungl@gmail.com>
2015-01-11 15:40:06 +0800

    Jump Game

---
+++ Jump Game.cpp
@@ -0,0 +1,20 @@
+class Solution {//method 1: bfs, Time Limit Exceeded
+public:
+    bool canJump(int A[], int n) {
+        if(A == NULL || n < 2) return true;
+		vector<int*> path;
+		path.push_back(A);
+		int* target = A + n;
+		while(!path.empty())
+		{
+			int* tmp = path[0];
+			for(int i = 1; i <= *tmp; i++)
+				if(tmp + i == target)
+					return true;
+				else
+					path.push_back(tmp+i);
+			path.erase(path.begin());
+		}
+		return false;
+    }
+};
